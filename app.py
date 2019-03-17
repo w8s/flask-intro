00314ee3673a7b3d3a1cfe01b87adb182af8fe3e
Todd Waits <todd.waits@gmail.com>
2019-03-17 12:10:54 -0400

    Add cli command, but the function does nothing... why?

--- app.py
+++ app.py
@@ -18,5 +18,17 @@ def new_name():
     return redirect(url_for("hello"))
 
 
+@app.cli.command('add_names')
+def cli_names():
+    """Adds five names to the names list""" 
+    print("Adding names...")
+    names.append("Kazoo")
+    names.append("Nathario")
+    names.append("Lizzymag")
+    names.append("Peppa")
+    names.append("Wesley")
+    print("Done adding names.")
+
+
 if __name__ == "__main__":
     app.run()
