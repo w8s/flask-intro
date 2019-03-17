48a56924e0dee4382b1717e7905272c0cad25801
Todd Waits <todd.waits@gmail.com>
2019-03-17 12:06:31 -0400

    Added form to the template and route to the app to add names and handle POST method.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+names = ["Todd"]
 
 
 @app.route("/")
 def hello():
-    return render_template("home.html", message="Hello World!")
+    return render_template("home.html", message="Hello World!", names=names)
+
+
+@app.route("/new-name/", methods=["POST"])
+def new_name():
+    name = request.form.get("name")
+    names.append(name)
+
+    return redirect(url_for("hello"))
 
 
 if __name__ == "__main__":
